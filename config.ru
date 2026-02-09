ece462b29df3641b14dcb1704d341f829a415a08
wiwi240 <william.mahi14@gmail.com>
2026-02-09 16:46:36 +0100

    add formulaire and submit button

--- config.ru
+++ config.ru
@@ -1,7 +1,8 @@
 require 'bundler'
 Bundler.require
 
+# Cette ligne est MAGIQUE : elle dit à Ruby d'aller chercher dans le dossier lib
 $:.unshift File.expand_path("./../lib", __FILE__)
-require 'controller'
 
+require 'controller' # Ruby va chercher lib/controller.rb grâce à la ligne du dessus
 run ApplicationController
--- lib/controller.rb
+++ lib/controller.rb
@@ -1,6 +1,19 @@
 class ApplicationController < Sinatra::Base
-    get '/' do
+
+# 1. Page d'accueil
+get '/' do
     erb :index
 end
-    run! if app_file == $0
+
+# 2. Route pour AFFICHER le formulaire (c'est ce qui te manquait !)
+get '/gossips/new/' do
+    erb :new_gossip
 end
+
+# 3. Route pour RECEVOIR les données du formulaire
+post '/gossips/new/' do
+    puts "Ce programme ne fait rien pour le moment, donc on va afficher un message dans le terminal"
+    "C'est reçu ! Regarde ton terminal."
+end
+
+end # Le SEUL et UNIQUE 'end' qui ferme la classe doit être ici, à la toute fin
