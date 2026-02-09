ea3cebfce9e8a6aceddf14d18f65d4c5d3d2cb64
wiwi240 <william.mahi14@gmail.com>
2026-02-09 16:24:18 +0100

    add code fonctionnel

--- lib/controller.rb
+++ lib/controller.rb
@@ -1,9 +1,6 @@
-require 'bundler'
-Bundler.require
-
 class ApplicationController < Sinatra::Base
     get '/' do
-    "<html><head><title>The Gossip Project</title></head><body><h1>Mon super site de gossip !</h1></body></html>"
+    erb :index
 end
     run! if app_file == $0
 end
